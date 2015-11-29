bdad0e9e299d4a9c7ea853d4a2cc09fd0b769a45
Dmitry Smirnov <dmitri.smirnov@gmail.com>
2015-11-29 13:35:48 +0100

    add autogeneration

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
-/* globals $ */
+/* globals $, st */
 
+window.st = window.st || {};
 
 $(function(){
 
@@ -12,4 +13,61 @@ $(function(){
 
 
     $('.st-panel').on('click', '.st-choise', selectBest);
-})
+
+
+
+    st.data = [
+        [
+            'datasets/aloe_2/aloe_2_conventional.png',
+            'datasets/aloe_2/aloe_2_true.png',
+            'datasets/aloe_2/aloe_2_matting.png',
+            'datasets/aloe_2/aloe_2_matting_inpainting.png'
+        ]
+    ];
+
+    var imgTpl = function(path){
+        return '<div class="st-choise"><img src="' + path + '"/></div>';
+    }
+
+    var tpl = function(steps){
+        var s = '';
+        var l = steps.length;
+
+        steps.forEach(function(step, i){
+            s += '<div class="st-step">';
+            s += '    <h1>Dataset # '+(i+1)+' from '+l+'</h1>';
+            s += '    <div class="st-panel clearfix">';
+
+            step.forEach(function(el){
+                s += imgTpl(el);
+            });
+
+            s += '   </div>';
+            s += '   <div align="right">';
+            s += '      <button>Next Dataset &gt;&gt;</button>';
+            s += '   </div>';
+            s += '</div>';
+        });
+
+        return s;
+    };
+
+
+
+    (function generate() {
+        $('.st-steps').html(tpl(st.data));
+
+    })();
+
+    // var steps = data.length // 1
+
+
+    // result = [
+    //     'datasets/aloe_2/aloe_2_conventional.png',
+
+    // ]
+
+
+
+
+});
