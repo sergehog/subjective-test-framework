e67ef1b34275fbff5b241375e0b1b51a49893291
Sergey <sergei.smirnov@gmail.com>
2015-11-29 12:44:31 +0200

    initial commit

---
+++ experiments.js
@@ -0,0 +1,4 @@
+{
+"datasets": ["aloe_2","aloe_3","aloe_4","aloe_5","aloe_6","moebius_2","moebius_3","moebius_4","moebius_5","moebius_6","reindeer_2","reindeer_3","reindeer_4","reindeer_5","reindeer_6"],
+"methods": ["conventional","layered","matting","matting_inpainting","true"]
+}
